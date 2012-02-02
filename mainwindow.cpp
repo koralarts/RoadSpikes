a1b2db1d015e3bb57cc6707ab9f6eebbdc9d5ee4
Luke Queenan <lukequeenan@gmail.com>
2012-02-02 13:09:46 -0800

    Changed tabs to spaces!!!

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,8 +13,8 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-	setupTable();
-	getIptable();
+    setupTable();
+    getIptable();
 }
 
 MainWindow::~MainWindow()
@@ -27,7 +27,7 @@ MainWindow::~MainWindow()
  **/
 void MainWindow::on_actionExit_triggered()
 {
-	this->close();
+    this->close();
 }
 
 /**
@@ -36,121 +36,121 @@ void MainWindow::on_actionExit_triggered()
  **/
 void MainWindow::setupTable()
 {
-	QStringList headers;
-	headers.append("Chain");
-	headers.append("Protocol");
-	headers.append("Source");
-	headers.append("Destination");
-	headers.append("SPort");
-	headers.append("DPort");
-	headers.append("Target");
-        headers.append("Miscellaneous");
+    QStringList headers;
+    headers.append("Chain");
+    headers.append("Protocol");
+    headers.append("Source");
+    headers.append("Destination");
+    headers.append("SPort");
+    headers.append("DPort");
+    headers.append("Target");
+    headers.append("Miscellaneous");
 
-	for(int i = 0; i < headers.size(); i++) {
-		this->ui->rulesView->insertColumn(i);
-		this->ui->rulesView->setHorizontalHeaderItem(i,
-										new QTableWidgetItem(headers.at(i)));
-	}
+    for(int i = 0; i < headers.size(); i++) {
+        this->ui->rulesView->insertColumn(i);
+        this->ui->rulesView->setHorizontalHeaderItem(i,
+                                                     new QTableWidgetItem(headers.at(i)));
+    }
 }
 
 void MainWindow::getIptable()
 {
-	QProcess *iptable = new QProcess();
-	QByteArray result;
-	//iptable->start("ls");
-	iptable->start("iptables", QStringList() << "-L");
+    QProcess *iptable = new QProcess();
+    QByteArray result;
+    //iptable->start("ls");
+    iptable->start("iptables", QStringList() << "-L");
 
-	//Check if iptable has started
-	if(!iptable->waitForStarted()) {
-		QMessageBox(QMessageBox::Critical, "Error", "Cannot Run iptables",
-					QMessageBox::Ok).show();
-	}
+    //Check if iptable has started
+    if(!iptable->waitForStarted()) {
+        QMessageBox(QMessageBox::Critical, "Error", "Cannot Run iptables",
+                    QMessageBox::Ok).show();
+    }
 
-	//Wait for data before reading
-	iptable->waitForReadyRead();
-	result = iptable->readAllStandardOutput();
+    //Wait for data before reading
+    iptable->waitForReadyRead();
+    result = iptable->readAllStandardOutput();
 
-	//Check if iptable finished
-	if(!iptable->waitForFinished()) {
-		QMessageBox(QMessageBox::Critical, "Error", "iptables stalled",
-					QMessageBox::Ok).show();
-	}
+    //Check if iptable finished
+    if(!iptable->waitForFinished()) {
+        QMessageBox(QMessageBox::Critical, "Error", "iptables stalled",
+                    QMessageBox::Ok).show();
+    }
 
-	parseResult(result);
+    parseResult(result);
 }
 
 void MainWindow::parseResult(QByteArray res)
 {
-	QString result(res);
-	QString chain;
-	QStringList lines = result.split("\n", QString::SkipEmptyParts);
-	int counter = 0;
+    QString result(res);
+    QString chain;
+    QStringList lines = result.split("\n", QString::SkipEmptyParts);
+    int counter = 0;
 
-	for(int i = 0; i < lines.size(); i++) {
-		//Check if name of chain
-		if(lines.at(i).contains("Chain")) {
-			chain = lines.at(i).split(" ").at(1);
-			counter = 0;
-		}
-		counter++;
-		//Check if rule
-		if(counter > 2) {
-			buildRule(chain, lines.at(i));
-		}
-	}
+    for(int i = 0; i < lines.size(); i++) {
+        //Check if name of chain
+        if(lines.at(i).contains("Chain")) {
+            chain = lines.at(i).split(" ").at(1);
+            counter = 0;
+        }
+        counter++;
+        //Check if rule
+        if(counter > 2) {
+            buildRule(chain, lines.at(i));
+        }
+    }
 }
 
 void MainWindow::buildRule(QString chain, QString line)
 {
-	QStringList rule;
-        QString miscellaneous;
-	int rowCount;
-	int portIndex;
+    QStringList rule;
+    QString miscellaneous;
+    int rowCount;
+    int portIndex;
 
-	rule = line.split(" ", QString::SkipEmptyParts);
-	rowCount = this->ui->rulesView->rowCount();
+    rule = line.split(" ", QString::SkipEmptyParts);
+    rowCount = this->ui->rulesView->rowCount();
 
-	this->ui->rulesView->insertRow(rowCount);
+    this->ui->rulesView->insertRow(rowCount);
 
-	this->ui->rulesView->setItem(rowCount, 0,
-								 new QTableWidgetItem(chain));
-        this->ui->rulesView->setItem(rowCount, this->ui->rulesView->columnCount() - 2,
-								 new QTableWidgetItem(rule.at(0)));
-	this->ui->rulesView->setItem(rowCount, 1,
-								 new QTableWidgetItem(rule.at(1)));
-	this->ui->rulesView->setItem(rowCount, 2,
-								 new QTableWidgetItem(rule.at(3)));
-	this->ui->rulesView->setItem(rowCount, 3,
-								 new QTableWidgetItem(rule.at(4)));
+    this->ui->rulesView->setItem(rowCount, 0,
+                                 new QTableWidgetItem(chain));
+    this->ui->rulesView->setItem(rowCount, this->ui->rulesView->columnCount() - 2,
+                                 new QTableWidgetItem(rule.at(0)));
+    this->ui->rulesView->setItem(rowCount, 1,
+                                 new QTableWidgetItem(rule.at(1)));
+    this->ui->rulesView->setItem(rowCount, 2,
+                                 new QTableWidgetItem(rule.at(3)));
+    this->ui->rulesView->setItem(rowCount, 3,
+                                 new QTableWidgetItem(rule.at(4)));
 
-	if((portIndex = rule.indexOf(QRegExp("^spt[s]?:.*"))) != -1) {
-		if(rule.at(portIndex).contains("spts")) {
-			this->ui->rulesView->setItem(rowCount, 4,
-										 new QTableWidgetItem(rule.at(portIndex).split("pts:").at(1)));
-		} else {
-			this->ui->rulesView->setItem(rowCount, 4,
-										 new QTableWidgetItem(rule.at(portIndex).split(":").at(1)));
-		}
-	}
-	if((portIndex = rule.indexOf(QRegExp("^dpt[s]?:.*"))) != -1) {
-		if(rule.at(portIndex).contains("dpts")) {
-			this->ui->rulesView->setItem(rowCount, 5,
-										 new QTableWidgetItem(rule.at(portIndex).split("pts:").at(1)));
-		} else {
-			this->ui->rulesView->setItem(rowCount, 5,
-										 new QTableWidgetItem(rule.at(portIndex).split(":").at(1)));
-		}
-	}
+    if((portIndex = rule.indexOf(QRegExp("^spt[s]?:.*"))) != -1) {
+        if(rule.at(portIndex).contains("spts")) {
+            this->ui->rulesView->setItem(rowCount, 4,
+                                         new QTableWidgetItem(rule.at(portIndex).split("pts:").at(1)));
+        } else {
+            this->ui->rulesView->setItem(rowCount, 4,
+                                         new QTableWidgetItem(rule.at(portIndex).split(":").at(1)));
+        }
+    }
+    if((portIndex = rule.indexOf(QRegExp("^dpt[s]?:.*"))) != -1) {
+        if(rule.at(portIndex).contains("dpts")) {
+            this->ui->rulesView->setItem(rowCount, 5,
+                                         new QTableWidgetItem(rule.at(portIndex).split("pts:").at(1)));
+        } else {
+            this->ui->rulesView->setItem(rowCount, 5,
+                                         new QTableWidgetItem(rule.at(portIndex).split(":").at(1)));
+        }
+    }
 
 }
 
 void MainWindow::on_addRuleButton_clicked()
 {
-	AddRule *rules = new AddRule;
+    AddRule *rules = new AddRule;
 
-	rules->show();
+    rules->show();
 
-	if(rules->result()) {
-		getIptable();
-	}
+    if(rules->result()) {
+        getIptable();
+    }
 }
