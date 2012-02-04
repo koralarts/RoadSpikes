46038d03fcc689f5e8c142e4782128f927e252a9
Luke Queenan <lukequeenan@gmail.com>
2012-02-03 16:21:47 -0800

    Starting on the script

--- shell/def_fw.sh
+++ shell/def_fw.sh
@@ -8,6 +8,13 @@ iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT DROP
 
+#BLOCK ALL EXTERNAL TRAFFIC TO PORTS 32768 - 32775 AND 137 - 139
+iptables -A INPUT -m multiport --sports 32768:32775,137:139 -j DROP
+
+#BLOCK ALL EXTERNAL TCP TRAFFIC TO PORTS 111 AND 515
+iptables -A INPUT -p tcp --dport 111 -j DROP
+iptables -A INPUT -p tcp --dport 515 -j DROP
+
 #ALLOW INBOUND/OUTBOUND tcp, udp, icmp FROM ALL PORTS
 iptables -A INPUT -p tcp -j ACCEPT
 iptables -A OUTPUT -p tcp -j ACCEPT
