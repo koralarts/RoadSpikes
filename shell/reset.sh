e7b0fdd4903a0677f26e72bff09fa317087ddce8
Karl Castillo <khcastillo@hotmail.com>
2012-02-06 15:30:09 -0800

    Fixed

--- shell/reset.sh
+++ shell/reset.sh
@@ -1,2 +1,12 @@
-route delete default gw 192.168.1.3 p3p1
-route delete -net 192.168.1.0 netmask 255.255.255.0 dev p3p1
+#route delete default gw 192.168.1.3 p3p1
+#route delete -net 192.168.1.0 netmask 255.255.255.0 dev p3p1
+
+iptables -F -t nat
+
+iptables -F
+iptables -X
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+
+echo "IPTables Reset"
