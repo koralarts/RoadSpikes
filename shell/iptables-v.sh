9d64b6e84263d64e75684a5b23e2978c4d7da8e4
Karl Castillo <khcastillo@hotmail.com>
2012-02-09 16:42:35 -0800

    fixed stuff again

--- shell/iptables-v.sh
+++ shell/iptables-v.sh
@@ -1,6 +1,10 @@
 while :
 do
 	clear
+	iptables -t mangle -L -v
+	echo "-----------------"
+	echo " NORMAL TABLE "
+	echo "----------------"
 	iptables -L -v
 	sleep 1
 done
--- shell/reset.sh
+++ shell/reset.sh
@@ -2,6 +2,7 @@
 #route delete -net 192.168.1.0 netmask 255.255.255.0 dev p3p1
 
 iptables -F -t nat
+iptables -F -t mangle
 
 iptables -F
 iptables -X
